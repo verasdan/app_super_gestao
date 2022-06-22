b1ee189a8c2edc43ae748c06468e898805141bfc
Dan <veras_dan@hotmail.com>
2022-06-22 15:00:03 -0300

    :pencil2: Adicionando prefix

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos' );
 Route::get('/contato', 'ContatoController@contato' );
 
 Route::get('/login', function(){ return 'Login'; });
-Route::get('/clientes', function(){ return 'Clientes'; });
-Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-Route::get('/produtos', function(){ return 'Produtos'; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+});
