aaa0bff05a04b256eca499ee432f9cf571827f83
Dan <veras_dan@hotmail.com>
2022-06-22 15:28:32 -0300

    :pencil2: Corrigindo ortografia / Add route

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal' );
-Route::get('/sobre-nos', 'SobreNosController@sobreNos' );
-Route::get('/contato', 'ContatoController@contato' );
+Route::get('/', 'PrincipalController@principal' )->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos' )->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato' )->name('site.contato');
 
-Route::get('/login', function(){ return 'Login'; });
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function(){ return 'Clientes'; });
-    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-    Route::get('/produtos', function(){ return 'Produtos'; });
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });
