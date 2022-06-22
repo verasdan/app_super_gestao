802472a0934310f518a6491a66ece946da03f159
Dan <veras_dan@hotmail.com>
2022-06-22 20:44:29 -0300

    :zap: Melhoria no código

--- resources/views/site/layouts/index.blade.php
+++ resources/views/site/layouts/index.blade.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('titulo')</title>
+    <title>Super Gestão - @yield('titulo')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
+
+    @include('site.layouts._partials.topo')
+
     @yield('conteudo')
+    
 </body>
 </html>
