2c8b45c44df71a7073661c2a0d15d0fcbfe82e85
Dan <veras_dan@hotmail.com>
2022-06-26 20:43:54 -0300

    :sparkler: Fillable - Model Fornecedor

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
-    //
+    use SoftDeletes;
+
+    // Seleciona a tabela desejada
+    protected $table = 'fornecedores';
+
+    protected $fillable = [
+        'nome', 'site', 'telefone', 'uf', 'email'
+    ];
 }
