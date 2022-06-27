b7d6b6ba0463fc79e74b9c14b70939d55a3f3836
Dan <veras_dan@hotmail.com>
2022-06-27 11:33:32 -0300

    :zap: perf: updating code

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
+
         return view('site.contato');
+        
+    }
+
+    public function salvar(Request $request) {
+
+        SiteContato::create($request->all());
+
     }
 }
