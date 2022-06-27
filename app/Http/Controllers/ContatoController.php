ace0614c75ab14a3c3574dbe443cd02e1db9bc01
Dan <veras_dan@hotmail.com>
2022-06-27 15:57:41 -0300

    :recycle: Refactoring code

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        return view('site.contato');
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
 
     }
 
